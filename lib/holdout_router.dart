642aa06c071727aebf5cb42c93c426897675c4db
zhengkanyan <zhengkanyan@etsnet.cn>
2022-07-01 22:32:54 +0800

    Give weather cubit and bloc apart

--- lib/holdout_router.dart
+++ lib/holdout_router.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_holdout/weather/bloc/weather_bloc.dart';
+import 'package:my_holdout/weather/cubit/weather_cubit.dart';
 import 'package:my_holdout/weather/domain/weather_repository.dart';
-import 'package:my_holdout/weather/presenter/weather_search_page.dart';
+import 'package:my_holdout/weather/bloc/weather_search_page.dart' as weatherBloc;
+import 'package:my_holdout/weather/cubit/weather_search_page.dart' as weatherCubit;
 
 import 'cookbook/animate_a_widget/main_screen.dart';
 
@@ -21,11 +23,22 @@ class HoldoutRouter extends StatelessWidget {
                 context,
                 MaterialPageRoute(
                     builder: (context) => BlocProvider(
+                      create: (context) =>
+                          WeatherCubit(FakeWeatherRepository()),
+                      child: weatherCubit.WeatherSearchPage(),
+                    ))),
+            child: Text('Go to Weather Search Cubit'),
+          ),
+          ElevatedButton(
+            onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => BlocProvider(
                           create: (context) =>
                               WeatherBloc(FakeWeatherRepository()),
-                          child: WeatherSearchPage(),
+                          child: weatherBloc.WeatherSearchPage(),
                         ))),
-            child: Text('Go to Weather Search'),
+            child: Text('Go to Weather Search Bloc'),
           ),
           ElevatedButton(
             onPressed: () => Navigator.push(
