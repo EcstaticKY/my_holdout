08e9e76d9d70dd034aac7ff6992e926aa8e7f8bc
zhengkanyan <zhengkanyan@etsnet.cn>
2022-07-03 10:24:00 +0800

    Add injection container

---
+++ lib/injection_container.dart
@@ -0,0 +1,43 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:get_it/get_it.dart';
+import 'package:my_holdout/number_trivia/data/number_trivia_local_data_source.dart';
+import 'package:my_holdout/number_trivia/data/number_trivia_remote_data_source.dart';
+import 'package:my_holdout/number_trivia/data/number_trivia_repository_impl.dart';
+import 'package:my_holdout/number_trivia/domain/get_concrete_number_trivia.dart';
+import 'package:my_holdout/number_trivia/domain/get_random_number_trivia.dart';
+import 'package:my_holdout/number_trivia/presentation/number_trivia_bloc.dart';
+import 'package:my_holdout/shared/network_info.dart';
+import 'package:my_holdout/shared/util/input_converter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  //! Features - Number Trivia
+  sl.registerFactory(() => NumberTriviaBloc(concrete: sl(), random: sl(), converter: sl()));
+
+  // Use cases
+  sl.registerLazySingleton(() => GetConcreteNumberTrivia(sl()));
+  sl.registerLazySingleton(() => GetRandomNumberTrivia(sl()));
+  
+  // Repository
+  sl.registerLazySingleton(() => NumberTriviaRepositoryImpl(
+      remoteDataSource: sl(),
+      localDataSource: sl(),
+      networkInfo: sl()));
+
+  // Data sources
+  sl.registerLazySingleton(() => NumberTriviaRemoteDataSourceImpl(client: sl()));
+  sl.registerLazySingleton(() => NumberTriviaLocalDataSourceImpl(sharedPreferences: sl()));
+
+  //! Core
+  sl.registerLazySingleton(() => InputConverter());
+  sl.registerLazySingleton(() => NetworkInfoImpl(checker: sl()));
+
+  //! External
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => DataConnectionChecker());
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:my_holdout/holdout_router.dart';
+import 'injection_container.dart' as di;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(MyApp());
 }
 
