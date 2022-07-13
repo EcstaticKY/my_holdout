d29f7b36a16241edccdd2a9dc47ab462d10c80fc
zhengkanyan <zhengkanyan@etsnet.cn>
2022-07-13 11:02:23 +0800

    Moor Task I —— Tables & Queries

--- lib/holdout_router.dart
+++ lib/holdout_router.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_holdout/number_trivia/presentation/pages/number_trivia_page.dart';
+import 'package:my_holdout/tasks_with_moor/data/moor_database.dart';
+import 'package:my_holdout/tasks_with_moor/presentation/home_page.dart';
 import 'package:my_holdout/weather/bloc/weather_bloc.dart';
 import 'package:my_holdout/weather/cubit/weather_cubit.dart';
 import 'package:my_holdout/weather/domain/weather_repository.dart';
-import 'package:my_holdout/weather/bloc/weather_search_page.dart' as weatherBloc;
-import 'package:my_holdout/weather/cubit/weather_search_page.dart' as weatherCubit;
+import 'package:my_holdout/weather/bloc/weather_search_page.dart'
+    as weatherBloc;
+import 'package:my_holdout/weather/cubit/weather_search_page.dart'
+    as weatherCubit;
+import 'package:provider/provider.dart';
 
 import 'cookbook/animate_a_widget/main_screen.dart';
 
@@ -16,7 +21,8 @@ class HoldoutRouter extends StatelessWidget {
       appBar: AppBar(
         title: Text("Welcome to ZKY's holdout"),
       ),
-      body: Center(child: Column(
+      body: Center(
+          child: Column(
         children: <Widget>[
           Spacer(),
           ElevatedButton(
@@ -24,10 +30,10 @@ class HoldoutRouter extends StatelessWidget {
                 context,
                 MaterialPageRoute(
                     builder: (context) => BlocProvider(
-                      create: (context) =>
-                          WeatherCubit(FakeWeatherRepository()),
-                      child: weatherCubit.WeatherSearchPage(),
-                    ))),
+                          create: (context) =>
+                              WeatherCubit(FakeWeatherRepository()),
+                          child: weatherCubit.WeatherSearchPage(),
+                        ))),
             child: Text('Go to Weather Search Cubit'),
           ),
           ElevatedButton(
@@ -47,9 +53,23 @@ class HoldoutRouter extends StatelessWidget {
             child: Text('Go to animate a widget across screens'),
           ),
           ElevatedButton(
-            onPressed: () => Navigator.push(
-                context, MaterialPageRoute(builder: (context) => NumberTriviaPage())),
+            onPressed: () => Navigator.push(context,
+                MaterialPageRoute(builder: (context) => NumberTriviaPage())),
             child: Text('Go to Number Trivia'),
+          ),
+          ElevatedButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Provider(
+                        create: (_) => AppDatabase(),
+                        child: MaterialApp(
+                          title: 'Tasks App',
+                          home: HomePage(),
+                        ),
+                      )),
+            ),
+            child: Text('Go to task list'),
           ),
           Spacer(),
         ],
---
+++ lib/tasks_with_moor/readme.md
@@ -0,0 +1,1 @@
+https://resocoder.com/2019/06/26/moor-room-for-flutter-tables-queries-fluent-sqlite-database/
